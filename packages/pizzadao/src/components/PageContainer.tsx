b5b0a9bdf6585bf84f9e1f2f891674d95b80dd84
nenadmitt <nenadmitt@gmail.com>
2024-12-21 00:03:06 +0100

    remove old pizzadao

--- packages/pizzadao/src/components/PageContainer.tsx
+++
@@ -1,7 +0,0 @@
-export const PageContainer = ({children}:any) => {
-  return (
-    <div className="pizza-container">
-       {children}
-    </div>
-  );
-};
--- packages/pizzadao/src/components/Utils.ts
+++
@@ -1,8 +0,0 @@
-export const shortedString = (str: string, len:number) => {
-
-    if (str && str.length <= len) {
-        return str;
-    }
-
-    return str.substring(0,4) + "..." + str.substring(str.length - 4);
-}
