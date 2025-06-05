927c335ea6c24d6992f37280ec6882f61e78537e
nenadmitt <nenadmitt@gmail.com>
2025-06-05 21:55:45 +0200

    add new mint-client

--- packages/subname-minting-website-template/src/components/Types.ts
+++ packages/subname-minting-website-template/src/components/Types.ts
@@ -5,3 +5,12 @@ export interface Subname {
     texts: Record<string, string>;
     addresses: Record<string, string>;
 }
+
+export interface EnsListing {
+    name: string
+    type: "l1" | "l2"
+    isVerified: boolean
+    l2Metadata?: {
+        isExpirable: boolean
+    }
+}
--- packages/subname-minting-website-template/src/components/useNamespaceClient.tsx
+++ packages/subname-minting-website-template/src/components/useNamespaceClient.tsx
@@ -1,69 +1,81 @@
-import { createNamespaceClient, L2Chain, Listing, MintRequest, MintTransactionParameters } from "namespace-sdk"
-import { Address, Hash, namehash } from "viem"
-import { mainnet, sepolia, baseSepolia } from "viem/chains"
-import { usePublicClient, useSignTypedData, useWalletClient } from "wagmi"
-import { AppEnv } from "../environment"
-import { getChainName } from "namespace-sdk"
+import { Address, Hash } from "viem";
+import { usePublicClient, useWalletClient } from "wagmi";
+import {
+  createMintClient,
+  MintTransactionRequest,
+  MintTransactionResponse,
+} from "@namespacesdk/mint-manager";
+import { useAppConfig } from "./AppConfigContext";
+import { base, baseSepolia, mainnet, optimism, sepolia } from "viem/chains";
 
+const alchemyToken = import.meta.env.VITE_APP_ALCHEMY_TOKEN;
+const appSource = import.meta.env.VITE_APP_MINT_SOURCE || "namespacesdk";
 
-const fullName = AppEnv.name
-const nameChainId = Number(AppEnv.chainId)
+const getRPCUrls = () => {
+  if (alchemyToken && alchemyToken.length > 0) {
+    return {
+      [base.id]: generateAlchemyUri("base-mainnet", alchemyToken),
+      [baseSepolia.id]: generateAlchemyUri("base-sepolia", alchemyToken),
+      [mainnet.id]: generateAlchemyUri("eth-mainnet", alchemyToken),
+      [sepolia.id]: generateAlchemyUri("eth-sepolia", alchemyToken),
+      [optimism.id]: generateAlchemyUri("opt-mainnet", alchemyToken),
+    };
+  }
+};
 
-export const LISTEN_NAME: Listing = {
-    fullName: fullName,
-    label: fullName.split('.')[0],
-    network: [sepolia.id, baseSepolia.id].includes(nameChainId as 11155111 | 84532) ? "sepolia" : "mainnet",
-    node: namehash(fullName),
-    listingType: [mainnet.id, sepolia.id].includes(nameChainId as 1 | 11155111) ? "sellUnruggable" : "l2",
-    registryNetwork: [mainnet.id, sepolia.id].includes(nameChainId as 1 | 11155111) ?  undefined: getChainName(nameChainId) as L2Chain
+const mintClient = createMintClient({
+  mintSource: appSource,
+  cursomRpcUrls: getRPCUrls(),
+});
+
+function generateAlchemyUri(alchemyNetwork: string, token: string) {
+  return `https://${alchemyNetwork}.g.alchemy.com/v2/${token}`;
 }
-
-const client = createNamespaceClient({
-    chainId: nameChainId,
-    //mintSource: "template",
-    mode: "production",
-})
 
 export const useNamepsaceClient = () => {
+  const { listingChainId, listingType, listedName } = useAppConfig();
 
-    const publicClient = usePublicClient({chainId: nameChainId})
-    const { data: walletClient } = useWalletClient({ chainId: nameChainId })
-    const { signTypedDataAsync } = useSignTypedData()
+  const publicClient = usePublicClient({ chainId: listingChainId });
+  const { data: walletClient } = useWalletClient({ chainId: listingChainId });
 
-    const checkAvailable = async (label: string) => {
-        return client.isSubnameAvailable(LISTEN_NAME, label)
+  const checkAvailable = async (label: string) => {
+    if (listingType === "l1") {
+      return mintClient.isL1SubnameAvailable(`${label}.${listedName}`);
     }
+    return mintClient.isL2SubnameAvailable(
+      `${label}.${listedName}`,
+      listingChainId
+    );
+  };
 
-    const mintParameters = async (req: MintRequest) => {
-        return client.getMintTransactionParameters(LISTEN_NAME, req)
-    }
+  const mintParameters = async (req: MintTransactionRequest) => {
+    return mintClient.getMintTransactionParameters(req);
+  };
 
-    const generateAuthToken = async (principal: Address) => {
-        return client.generateAuthToken(principal, signTypedDataAsync, "Generate token");
-    }
+  const executeTx = async (
+    mintTxParams: MintTransactionResponse,
+    minter: Address
+  ) => {
+    const { request } = await publicClient!.simulateContract({
+      abi: mintTxParams.abi,
+      address: mintTxParams.contractAddress,
+      functionName: mintTxParams.functionName,
+      account: minter,
+      args: mintTxParams.args,
+      value: mintTxParams.value,
+    });
 
-    const executeTx = async (mintTxParams: MintTransactionParameters, minter: Address) => {
-        const {request} = await publicClient!.simulateContract({
-            abi: mintTxParams.abi,
-            address: mintTxParams.contractAddress,
-            functionName: mintTxParams.functionName,
-            account: minter,
-            args: mintTxParams.args,
-            value: mintTxParams.value
-        })
+    return walletClient!.writeContract(request);
+  };
 
-        return walletClient!.writeContract(request);
-    }
+  const waitForTx = async (hash: Hash) => {
+    await publicClient!.waitForTransactionReceipt({ hash, confirmations: 2 });
+  };
 
-    const waitForTx = async (hash: Hash) => {
-        await publicClient!.waitForTransactionReceipt({hash, confirmations: 2})
-    }
-    
-    return {
-        waitForTx,
-        executeTx,
-        checkAvailable,
-        mintParameters,
-        generateAuthToken
-    }
-}
+  return {
+    waitForTx,
+    executeTx,
+    checkAvailable,
+    mintParameters,
+  };
+};
